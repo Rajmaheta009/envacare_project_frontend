4f729ca606aee9b4467692f17a6e23743515acbc
RaJ <rajmaheta2008@gmail.com>
2025-07-25 12:41:42 +0530

    env in set host and peramter add file in have set that host

--- pdf_converter_files/qoutation_invoice_maker.py
+++ pdf_converter_files/qoutation_invoice_maker.py
@@ -9,5 +9,5 @@ def quotation_pdf_maker(c_id,q_id):
     # If no variables needed, just render as-is
     html_rendered = template.render()
 
-    HTML(string=html_rendered, base_url='.').write_pdf('envacare_quotation_with_watermark.pdf')
+    HTML(string=html_rendered, base_url='.').write_pdf('../envacare_quotation_with_watermark.pdf')
     print("✅ PDF generated: envacare_quotation_with_watermark.pdf")
